f3788a2dc19a0453d921bf73a4b4411177887bc3
apple6346654 <apple6346654@naver.com>
2025-03-18 16:32:52 +0900

    스프링 기본 스코프와 Provider

---
+++ core/src/main/java/hello/core/web/LogDemoContoroller.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoContoroller {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody // 문자로 그대로 응답가능
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString(); //고객이 어떤 URL로 요청했는지 알 수 있음
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("conroller test");
+        logDemoService.logic("testId");
+        return "ok";
+    }
+
+
+}
