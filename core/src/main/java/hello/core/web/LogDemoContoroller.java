4895d9371d05b03d9106c5da08e252a09af680b4
apple6346654 <apple6346654@naver.com>
2025-03-19 23:07:02 +0900

    스프링 기본 스코프와 Provider 확인

--- core/src/main/java/hello/core/web/LogDemoContoroller.java
+++ core/src/main/java/hello/core/web/LogDemoContoroller.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,11 +15,12 @@ import java.util.logging.Logger;
 public class LogDemoContoroller {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody // 문자로 그대로 응답가능
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString(); //고객이 어떤 URL로 요청했는지 알 수 있음
         myLogger.setRequestURL(requestURL);
 
@@ -26,6 +28,19 @@ public class LogDemoContoroller {
         logDemoService.logic("testId");
         return "ok";
     }
+
+    @RequestMapping("log-demo")
+    @ResponseBody // 문자로 그대로 응답가능
+    public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString(); //고객이 어떤 URL로 요청했는지 알 수 있음
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("conroller test");
+        logDemoService.logic("testId");
+        return "ok";
+    }
+
 
 
 }
